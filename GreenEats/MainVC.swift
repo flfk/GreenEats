0cb6ad8d913502df72e03e9ab0d53b224f0ad811
flfk <flfk@users.noreply.github.com>
2017-07-22 18:09:56 +1000

    loading screen set up

--- GreenEats/MainVC.swift
+++ GreenEats/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  GreenEats
 //
 //  Created by Felix Kramer on 22/7/17.
@@ -8,16 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
