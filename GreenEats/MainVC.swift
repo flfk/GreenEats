d821e7a6c4631ce7d238d62dc583f7c9a4f1c8b6
flfk <flfk@users.noreply.github.com>
2017-07-22 20:42:04 +1000

    added helper methods to MainVC

--- GreenEats/MainVC.swift
+++ GreenEats/MainVC.swift
@@ -11,14 +11,33 @@ import UIKit
 //import Core Data
 import CoreData
 
-class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+//add protocols for tableview data source and delegate, NS fetched results controller delegate
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
     
     //MARK: - Properties
     
     @IBOutlet weak var recipeTableView: UITableView!
     
     //MARK: - Core Data Stack Properties
+    
+    fileprivate lazy var fetchedResultsController: NSFetchedResultsController<Recipe> = {
+        
+        //create fetch request
+        let fetchRequest: NSFetchRequest<Recipe> = Recipe.fetchRequest()
+        
+        //configure fetch request
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        
+        //create fetch request controller
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: adManagedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        //configure fetch request controller
+        fetchedResultsController.delegate = self
+        
+        return fetchedResultsController
+    }()
+    
+    var managedObjectContext: NSManagedObjectContext?
     
     //MARK: - View Life Cycle
     
@@ -35,9 +54,64 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     //MARK: - Navigation
     
+    //MARK: - Actions
+    
     //MARK: - Helper Methods
     
-    //MARK: - Actions
+    //UpdateView updates the user interface
+    fileprivate func updateView() {
+        var hasRecipes = false
+        
+        if let recipes = fetchedResultsController.fetchedObjects {
+            hasRecipes = recipes.count > 0
+        }
+        
+        recipeTableView.isHidden = !hasRecipes
+        print("Update View Function")
+    }
+    
+    //if there are no recipes display setup label
+    private func setupLabel() {
+        
+        //XX Placeholder if setup label / view / image to be added later (see evernote)
+        
+    }
+    
+    private func setupView() {
+        setupLabel()
+        
+        updateView()
+    }
+    
+    //configureRecipeCell updates the cell contents
+    func configureRecipeCell(_ cell: RecipeCell, at indexPath: IndexPath) {
+        
+        //display status
+        print("configuring recipe cell")
+        
+        //fetch recipe
+        let recipe = fetchedResultsController.object(at: indexPath)
+        
+        //configure cell contents
+        //XX PLACEHOLDER
+        cell.recipeNameLbl.text = recipe.name
+        cell.recipeEmmissionsLbl.text = "\(recipe.emissions)"
+        cell.recipeEmojiImg.image = UIImage(named: recipe.rating!)
+    }
+    
+    //attemptFetch fetches the data from the fetched results controller
+    func attemptFetch() {
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            let error = error as NSError
+            print("\(error)")
+        }
+        
+        updateView()
+        
+    }
     
     //MARK: - Table View DataSource Protocol Functions
     
