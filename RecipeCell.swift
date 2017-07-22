a8b4c6ee69d4abf4913bdd2caa70317565d662d0
flfk <flfk@users.noreply.github.com>
2017-07-22 18:37:51 +1000

    RecipeVC initial set up

---
+++ RecipeCell.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeCell.swift
+//  GreenEats
+//
+//  Created by Felix Kramer on 22/7/17.
+//  Copyright © 2017 Felix Kramer. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
