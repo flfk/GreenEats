4b1dec28e938f95cfaad83f5e03444e8e5a5b368
flfk <flfk@users.noreply.github.com>
2017-07-25 10:46:23 +1000

    beginning to implement picker view

--- SelectIngredientVC.swift
+++ SelectIngredientVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SelectIngredientVC: UIViewController {
+class SelectIngredientVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     
     //MARK: - Properties
@@ -22,12 +22,46 @@ class SelectIngredientVC: UIViewController {
     @IBOutlet weak var ingredientRatingImg: UIImageView!
     
     @IBOutlet weak var ingredientEmissionsLbl: UILabel!
+    
+    
+    //MARK: - Placeholder Quantity Data
+    
+    let placeholderQuantity = [[1,2,3,4,5,6],[0,1/4,1/2,3/4],["each","grams"]]
+    
+    
     //MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //set picker view delegate and datasource to self
+        ingredientQuantityPicker.delegate = self
+        ingredientQuantityPicker.dataSource = self
 
-        // Do any additional setup after loading the view.
+       
+    }
+    
+    //MARK: - Picker View Data Source and Delegate Protocol Functions
+    
+    
+    //----------CHECK OUT THIS TUTORIAL https://www.youtube.com/watch?v=6Qd3CdWYeJ8 ---------------------
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return placeholderQuantity.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+      
+        return placeholderQuantity[component].count
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(placeholderQuantity[component])"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        return
     }
 
 }
